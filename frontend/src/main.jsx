ad6dde77af508c9b1568a1ddc17dfd575268ff06
harshaggarwal9 <harshaggarwal935@gmail.com>
2025-08-30 16:09:10 +0530

    finl deployment1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 // import { AuthContextProvider } from "./context/AuthContext.jsx";
-import {AuthContextProvider} from "./context/AuthContext.jsx"
-import { SocketContextProvider } from "./context/SocketContext.jsx";
+import {AuthContextProvider} from "./context/AuthContext"
+import { SocketContextProvider } from "./context/SocketContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
